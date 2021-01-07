54a9fe8f42ed34d5118606193c39a4c4cc8a6682
Jaja Brown <65190496+jbrown827@users.noreply.github.com>
2021-01-06 23:44:35 -0600

    update scripts

--- script.js
+++ script.js
@@ -1,46 +1,82 @@
-//Eventually chang this variable to mty string
-//will take in user input
-var cityName = "new orleans";
-var key = "2c227a7f49d0a229e5a38ad3634b45c9";
-var queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=imperial&appid=${key}`;
-var urlQuery2 = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&units=imperial&appid=${key}`;
+//User input from form
+$("#button-submit").click((event) => {
+  event.preventDefault();
 
-$.ajax({
-  url: queryURL,
-  method: "GET",
-}).then(function (response) {
-  console.log(response);
-  console.log(response.name);
+  //Eventually chang this variable to mty string
+  //will take in user input
+  var cityName = $("#location-input").val();
+  var key = "2c227a7f49d0a229e5a38ad3634b45c9";
+  var queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=imperial&appid=${key}`;
+  var urlQuery2 = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&units=imperial&appid=${key}`;
 
-  var currentLoc = response.name;
-  var currentTemp = response.main.temp;
-  var currentHumid = response.main.humidity;
-  var windSpd = response.wind.speed;
+  var recentSearch = [];
 
-  $("#location").text(currentLoc);
-  $("#temp").text("Temperature: " + currentTemp);
-  $("#humid").text("Humidity: " + currentHumid + "%");
-  $("#wind-spd").text("Wind Speed: " + windSpd + " MPH");
-});
+  var searchedLocations = {
+    location: cityName,
+  };
+  searchList(recentSearch, cityName);
 
-$.ajax({
-  url: urlQuery2,
-  method: "GET",
-}).then((val) => {
-  console.log(val);
+  function searchList(arr, input) {
+    arr.push(input);
+    for (var i = 0; i < arr.length; i++) {
+      var srchLoc = arr[i];
 
-  var list = val.list;
-  var dayArray = [];
+      var li = $("<li>");
+      li.text(srchLoc);
 
-  for (var i = 0; i < list.length; i++) {
-    var converted = convertUnixTime(list[i].dt);
-
-    if (converted === "6 AM") {
-      dayArray.push(list[i]);
+      $("#search-list").append(li);
     }
   }
 
-  console.log(dayArray);
+  localStorage.setItem("location", JSON.stringify(searchedLocations));
+
+  //
+  /*---- ajax call for current weather ----*/
+  //
+
+  $.ajax({
+    url: queryURL,
+    method: "GET",
+  }).then(function (response) {
+    console.log(response);
+
+    var currentLoc = response.name;
+    var currentTemp = response.main.temp;
+    var currentHumid = response.main.humidity;
+    var windSpd = response.wind.speed;
+
+    $("#location").text(currentLoc);
+    $("#temp").text("Temperature: " + currentTemp);
+    $("#humid").text("Humidity: " + currentHumid + "%");
+    $("#wind-spd").text("Wind Speed: " + windSpd + " MPH");
+  });
+
+  //
+  /*---- ajax call for 5-day forecast ----*/
+  //
+
+  $.ajax({
+    url: urlQuery2,
+    method: "GET",
+  }).then((val) => {
+    console.log(val);
+
+    var list = val.list;
+    var dayArray = [];
+
+    for (var i = 0; i < list.length; i++) {
+      var converted = convertUnixTime(list[i].dt);
+      if (converted === "6 AM") {
+        dayArray.push(list[i]);
+      }
+    }
+
+    // console.log(dayArray[i]);
+  });
+
+  $("#location-input").val("");
+
+  var recents = JSON.parse(localStorage.getItem("location"));
 });
 
 function convertUnixTime(time) {
