291e9d5b7f63029ad73b57965718faebb1311890
Jaja Brown <65190496+jbrown827@users.noreply.github.com>
2021-03-18 03:09:14 -0500

    Saving to local storage and rendering to page from storage

--- script.js
+++ script.js
@@ -1,77 +1,85 @@
-//User input from form
-$("#button-submit").click((event) => {
-  event.preventDefault();
-
-  var cityName = $("#location-input").val();
+$(document).ready(function () {
+  //
+  //
+  //? Global Variables
 
   var key = "2c227a7f49d0a229e5a38ad3634b45c9";
-  var queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=imperial&appid=${key}`;
-  var urlQuery2 = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&units=imperial&appid=${key}`;
+  // var queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=imperial&appid=${key}`;
+  // var urlQuery2 = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&units=imperial&appid=${key}`;
+  var cityName = "";
+  var cities = [];
 
   //
   //
 
-  var recentSearch = [];
-
-  var searchedLocations = {
-    location: cityName,
-  };
-
-  searchList(recentSearch, cityName);
-
-  function searchList(arr, input) {
-    arr.push(input);
-    for (var i = 0; i < arr.length; i++) {
-      var srchLoc = arr[i];
-
-      var p = $("<p>");
-      p.addClass("src-cities");
-      p.text(srchLoc);
-
-      $("#cities-list").append(p);
+  function renderCities() {
+    $("#cities-list").empty();
+    for (var i = 0; i < cities.length; i++) {
+      var li = $("<li>");
+      li.addClass("city-items");
+      li.attr("data-index", cities[i]);
+      li.text(cities[i]);
+      $("#cities-list").prepend(li);
     }
   }
 
-  // Setting and getting local storage
-  localStorage.setItem("location", JSON.stringify(searchedLocations));
+  function storeCities() {
+    localStorage.setItem("cities", JSON.stringify(cities));
+  }
 
-  var recents = JSON.parse(localStorage.getItem("location"));
+  $("#button-submit").click((event) => {
+    event.preventDefault();
+    cityName = $("#location-input").val().trim();
+    cities.push(cityName);
+    $("#location-input").val("");
 
-  //
+    storeCities();
+    renderCities();
+    getWeather(cityName);
+    // getFiveDay(cityName);
+  });
+
   /*---- ajax call for current weather ----*/
+  function getWeather(location) {
+    $.ajax({
+      url:
+        "https://api.openweathermap.org/data/2.5/weather?q=" +
+        location +
+        "&units=imperial&appid=" +
+        key,
+      method: "GET",
+    }).then(function (response) {
+      console.log(response);
+
+      var currentLoc = response.name;
+      var currentDate = moment().format("M/D/YY");
+      var currentTemp = response.main.temp;
+      var currentHumid = response.main.humidity;
+      var windSpd = response.wind.speed;
+      var wthIcon = response.weather[0].icon;
+      var icon = "https://openweathermap.org/img/w/" + wthIcon + ".png";
+
+      //rendering text and attributes
+      //of the HTML elements
+      $("#location").text(currentLoc);
+      $("#loc-date").text(currentDate);
+      $("#icon1").attr("src", icon);
+      $("#temp").text(`Temperature: ${currentTemp}`);
+      $("#humid").text(`Humidity: ${currentHumid}`);
+      $("#wind-spd").text(`Wind Speed: ${windSpd} MPH`);
+
+      //variables for lat and lon
+      var lat = response.coord.lat;
+      var lon = response.coord.lon;
+    });
+    getUvIndex(lat, lon);
+  }
+
+  //
+  /*---- ajax call for UV index ----*/
   //
 
-  $.ajax({
-    url: queryURL,
-    method: "GET",
-  }).then(function (response) {
-    console.log(response);
-
-    var currentLoc = response.name;
-    var currentDate = moment().format("M/D/YY");
-    var currentTemp = response.main.temp;
-    var currentHumid = response.main.humidity;
-    var windSpd = response.wind.speed;
-    var wthIcon = response.weather[0].icon;
-    var icon = "https://openweathermap.org/img/w/" + wthIcon + ".png";
-
-    //rendering text and attributes
-    //of the HTML elements
-    $("#location").text(currentLoc);
-    $("#loc-date").text(currentDate);
-    $("#icon1").attr("src", icon);
-    $("#temp").text(`Temperature: ${currentTemp}`);
-    $("#humid").text(`Humidity: ${currentHumid}`);
-    $("#wind-spd").text(`Wind Speed: ${windSpd} MPH`);
-
-    //variables for lat and lon
-    var lat = response.coord.lat;
-    var lon = response.coord.lon;
-
-    //
-    /*---- ajax call for UV index ----*/
-    //
-
+  function getUvIndex(lat, lon) {
     $.ajax({
       url: `https://api.openweathermap.org/data/2.5/uvi?lat=${lat}&lon=${lon}&appid=${key}`,
       method: "GET",
@@ -99,50 +107,55 @@ $("#button-submit").click((event) => {
         $(".uv").css("color", "white");
       }
     });
-  });
+  }
 
   //
   /*---- ajax call for 5-day forecast ----*/
   //
+  function getFiveDay(city) {
+    $.ajax({
+      url:
+        "https://api.openweathermap.org/data/2.5/forecast?q=" +
+        city +
+        "&units=imperial&appid=" +
+        key,
+      method: "GET",
+    }).then((val) => {
+      //console.log(val);
 
-  $.ajax({
-    url: urlQuery2,
-    method: "GET",
-  }).then((val) => {
-    //console.log(val);
+      var data = val.list;
 
-    var data = val.list;
+      for (var i = 0; i < data.length; i++) {
+        //local variable declared to convert
+        // unix timestamp to UTC time
+        var time = moment(data[i].dt * 1000).format("H a");
 
-    for (var i = 0; i < data.length; i++) {
-      //local variable declared to convert
-      // unix timestamp to UTC time
-      var time = moment(data[i].dt * 1000).format("H a");
+        if (time === "6 am") {
+          //
+          //local variables to populate the text of the HTML document
+          //convert the date from a unix timestamp
+          var forDate = moment(data[i].dt_txt).format("M/D/YY");
+          var forTemp = data[i].main.temp;
+          var forHumid = data[i].main.humidity;
+          var imgIcon = data[i].weather[0].icon;
+          var forIcon = "https://openweathermap.org/img/w/" + imgIcon + ".png";
 
-      if (time === "6 am") {
-        //
-        //local variables to populate the text of the HTML document
-        //convert the date from a unix timestamp
-        var forDate = moment(data[i].dt_txt).format("M/D/YY");
-        var forTemp = data[i].main.temp;
-        var forHumid = data[i].main.humidity;
-        var imgIcon = data[i].weather[0].icon;
-        var forIcon = "https://openweathermap.org/img/w/" + imgIcon + ".png";
-
-        //variable to populate the card content
-        //using template literal
-        var html = `
+          //variable to populate the card content
+          //using template literal
+          var html = `
         <div class="card">
         <div class="card-body">
           <p class="for-date">${forDate}</p>
           <img src="${forIcon}" alt="weather icon" class="forecast-icon" />
           <p class="for-temp">Temp: ${forTemp}</p>
           <p class="for-humid"> Humidity: ${forHumid}</p>
+        </div>
         </div>`;
 
-        //appending card to the HTML document
-        $("#forecast-cards").append(html);
+          //appending card to the HTML document
+          $("#forecast-cards").append(html);
+        }
       }
-    }
-  });
-  $("#location-input").val("");
+    });
+  }
 });
